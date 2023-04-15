9665a38444a0ff001771ef19244c1c1db5552a01
sainathpoojary <spoojary614@gmail.com>
2023-04-16 02:50:20 +0530

    :bug: Port Error Fixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
